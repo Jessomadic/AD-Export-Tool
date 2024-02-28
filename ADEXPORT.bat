844c0b9568ec501f38855fbb82095e765c73c173
Jesse <35648348+Jessomadic@users.noreply.github.com>
2024-02-28 13:03:05 -0500

    Update ADEXPORT.bat

--- ADEXPORT.bat
+++ ADEXPORT.bat
@@ -1,4 +1,4 @@
 Echo Please Wait This May Take A While
-powershell.exe -ExecutionPolicy Unrestricted -command "S:\Cust_Serv\Help` Desk` Info\Help` Desk` PC` Setup` Docs\HD` Fixes\AD` EXPORT` TOOL\Scripts\Bothexporter.PS1"
+powershell.exe -ExecutionPolicy Unrestricted -command "*POINT/TO/SCRIPT/FOLDER*"
 Echo All Exported to Export Folder
 C:\Users\%username%\desktop\Export.CSV
