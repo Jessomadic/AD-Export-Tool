6948c93ea0e04c996d22eab019c4be2e32145492
Jesse <35648348+Jessomadic@users.noreply.github.com>
2024-02-28 13:02:19 -0500

    Add files via upload

---
+++ ADEXPORT.bat
@@ -0,0 +1,4 @@
+Echo Please Wait This May Take A While
+powershell.exe -ExecutionPolicy Unrestricted -command "S:\Cust_Serv\Help` Desk` Info\Help` Desk` PC` Setup` Docs\HD` Fixes\AD` EXPORT` TOOL\Scripts\Bothexporter.PS1"
+Echo All Exported to Export Folder
+C:\Users\%username%\desktop\Export.CSV
